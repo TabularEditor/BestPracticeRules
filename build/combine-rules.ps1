98eda7d4d863149cd009d3090931c8c4250d932c
Daniel Otykier <otykier@gmail.com>
2017-11-11 20:41:47 +0100

    Combine script

---
+++ build/combine-rules.ps1
@@ -0,0 +1,19 @@
+﻿$standardRules = @()
+$contribRules = @()
+
+Get-ChildItem -Recurse -Filter *.json |
+    ForEach-Object {
+        $src = $_.DirectoryName.Substring($_.DirectoryName.IndexOf("\src") + 5)
+        $ruleJson = Get-Content -Raw -Path $_.FullName
+        $rule = ConvertFrom-Json $ruleJson
+
+        $rule | Add-Member Source $src
+
+        if($src.StartsWith("standard")) { $standardRules += $rule }
+        if($src.StartsWith("contrib")) { $contribRules += $rule }
+    }
+
+$jsonStandard = ConvertTo-Json $standardRules
+$jsonContrib = ConvertTo-Json $contribRules
+Set-Content "BPARules-standard.json" $standardRules
+Set-Content "BPARules-contrib.json" $contribRules
